6ed7ef11e1e095446f36d31ba176993b75841d55
Haseebcodejourney <hamzaneuraltec@gmail.com>
2024-12-10 12:00:48 +0200

    update

---
+++ src/components/HeroCommon.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Wrapper } from '../styles/herocommon/Style'
+
+export default function HeroCommon() {
+  return (
+    <>
+        <Container>
+            <Wrapper>
+                
+            </Wrapper>
+        </Container>
+    </>
+  )
+}
---
+++ src/styles/herocommon/Style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  width: 100%;
+  
+  @media screen and (max-width: 767px){
+  }
+  
+`;
+
+export const Wrapper = styled.div`
+  max-width: 1240px;
+  width: 100%;
+  @media screen and (max-width: 767px){
+    padding-left: 24px;
+    padding-right: 24px;
+  }
+  
+`;
+
+export const H2 = styled.h2`
+  font-size: 36px;
+  padding: 4px 0px;
+  margin-bottom: 1.5rem;
+`;
+
+
+
+
