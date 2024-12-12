73dfcc4fc0797372534d17e05acbadaedcdd1286
Haseebcodejourney <hamzaneuraltec@gmail.com>
2024-12-12 12:12:04 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
-import { ButtonsPrimary, ButtonsSecondary, ButtonsWrapper, Figure, Logo, MenuItems, MenuList, Wrapper, Image } from '../styles/header/Style'
+import React, { useState } from 'react';
+import { ButtonsPrimary, ButtonsSecondary, ButtonsWrapper, Figure, Logo, MenuItems, MenuList, Wrapper, Image, Hamburger, MobileMenu } from '../styles/header/Style';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-    <Wrapper>
+      <Wrapper>
         <Logo>
           <Figure>
             <Image src='https://cloudsafaris.com/combo-logo-white.png' alt='Logo' width={206} height={24} />
           </Figure>
         </Logo>
+
+        {/* Mobile Hamburger Icon */}
+        <Hamburger onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </Hamburger>
+
+        {/* Menu List */}
         <MenuList>
           <MenuItems>Trips</MenuItems>
           <MenuItems>Hotels & Lodges</MenuItems>
           <MenuItems>Destinations</MenuItems>
         </MenuList>
+
         <ButtonsWrapper>
           <ButtonsPrimary>Find Your Trip</ButtonsPrimary>
           <ButtonsSecondary>Sign in</ButtonsSecondary>
         </ButtonsWrapper>
-        </Wrapper>
+      </Wrapper>
+
+      {/* Mobile Menu Dropdown */}
+      {isMenuOpen && (
+        <MobileMenu>
+          <MenuItems>Trips</MenuItems>
+          <MenuItems>Hotels & Lodges</MenuItems>
+          <MenuItems>Destinations</MenuItems>
+        </MobileMenu>
+      )}
     </>
-  )
+  );
 }
--- src/styles/header/Style.js
+++ src/styles/header/Style.js
@@ -1,22 +1,4 @@
-// Import styled-components
 import styled from "styled-components";
-
-export const Container = styled.section`
-  max-width: 1280px;
-  width: 100%;
-  position: absolute;
-    display: flex;
-    justify-content: space-between;
-    margin: 0 auto;
-
-  @media (max-width: 768px) {
-    /* flex-direction: column;
-    gap: 9rem;
-    padding-top: 9rem;
-    padding-bottom: 9rem; */
-  }
-`;
-
 
 export const Wrapper = styled.div`
   display: flex;
@@ -30,55 +12,125 @@ export const Wrapper = styled.div`
   max-width: 1152px;
   left: 0;
   right: 0;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 2rem;
+    justify-content: center;
+    padding-top: 2rem;
+  }
 `;
 
-
 export const Logo = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Figure = styled.figure`
-margin: 0px;
+  margin: 0;
 `;
 
 export const Image = styled.img`
-
+  width: 100%;
 `;
 
 export const MenuList = styled.ul`
-list-style: none;
-padding: 0px;
-margin: 0px;
-display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const MenuItems = styled.li`
-padding: 8px 16px;
-color: #fff;
-font-size: 14px;
+  padding: 8px 16px;
+  color: #fff;
+  font-size: 14px;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
 `;
 
-
 export const ButtonsWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 10px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 1rem;
+  }
 `;
 
 export const ButtonsPrimary = styled.div`
-font-size: 14px;
-padding: 8px 16px;
-color: #fff;
-background-color: #fff3;
-border-radius: .5rem;
+  font-size: 14px;
+  padding: 8px 16px;
+  color: #fff;
+  background-color: #fff3;
+  border-radius: .5rem;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+    padding: 12px 24px;
+  }
 `;
 
 export const ButtonsSecondary = styled.div`
-font-size: 14px;
-padding: 8px 16px;
-color: #fff;
-background-color: #212121;
-border-radius: .5rem;
+  font-size: 14px;
+  padding: 8px 16px;
+  color: #fff;
+  background-color: #212121;
+  border-radius: .5rem;
+
+  @media (max-width: 768px) {
+    font-size: 16px;
+    padding: 12px 24px;
+  }
+`;
+
+export const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 24px;
+  width: 30px;
+  cursor: pointer;
+  z-index: 100;
+
+  div {
+    width: 25px;
+    height: 3px;
+    background-color: #fff;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #212121;
+  position: absolute;
+  top: 70px; /* Adjust based on header size */
+  left: 0;
+  width: 100%;
+  padding: 20px 0;
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+
+  ${MenuItems} {
+    padding: 12px;
+    font-size: 18px;
+  }
 `;
