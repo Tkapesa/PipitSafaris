de1ce47989e9b3cc45952e583e8da0b24c0b4bef
Tkapesa <kapesatakunda026@gmail.com>
2025-01-02 16:07:57 +0300

    update test

--- src/styles/Affiliates/Background.js
+++ src/styles/Affiliates/Background.js
@@ -1,44 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-    width: 100%;                        
-    display: flex;             
-    align-items: center;        
-    justify-content: center; 
-    flex-direction: column ;
-    box-sizing: border-box; 
-    margin: 0 auto;
-    padding-bottom: 0rem;
-    background: #E8E8E8;
-    margin-bottom: 72px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+  box-sizing: border-box;
+  margin: 0 auto;
+  padding-bottom: 0rem;
+  background: #e8e8e8;
+  margin-bottom: 72px;
+  box-sizing: border-box;
+  background: ${(props) => (props.theme === "dark" ? "#001730" : "#f6f6f6")};
+  @media screen and (max-width: 1024px) {
+    padding-top: 6rem;
+    padding-bottom: 6rem;
     box-sizing: border-box;
-    background: ${(props) => (props.theme === 'dark' ? '#001730' : '#f6f6f6')};
-    @media screen and (max-width: 1024px){
-      padding-top: 6rem;
-      padding-bottom: 6rem;
-      box-sizing: border-box; 
-    }
+  }
 `;
 
 export const Wrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    flex-direction: row-reverse;
-    justify-content: space-between;
-    max-width: 1220px;
-    width: 100%;
-    margin: 0 auto;
-    padding: 48px 32px;
-    @media screen and (max-width: 1024px){
-      gap:6rem;
-      justify-content: center;
-      flex-direction: column-reverse !important;
-      padding: 96px 32px;
-      align-items: center;
-      box-sizing: border-box; 
-}
-
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  max-width: 1220px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 48px 32px;
+  @media screen and (max-width: 1024px) {
+    gap: 6rem;
+    justify-content: center;
+    flex-direction: column-reverse !important;
+    padding: 96px 32px;
+    align-items: center;
+    box-sizing: border-box;
+  }
 `;
 
 export const Left = styled.div`
@@ -50,9 +50,8 @@ export const Left = styled.div`
   max-width: 840px;
   width: 100%;
   text-align: left;
-  @media screen and (max-width: 1024px){
-   
-}
+  @media screen and (max-width: 1024px) {
+  }
 `;
 
 export const Right = styled.div`
@@ -62,34 +61,32 @@ export const Right = styled.div`
   justify-content: space-between;
   max-width: 280px;
   width: 100%;
-  @media screen and (max-width: 1024px){
+  @media screen and (max-width: 1024px) {
     max-width: 100%;
-}
+  }
 `;
 
 export const Figure = styled.figure`
   margin: 0px;
-  @media screen and (max-width:767px){
-
-}
-  
+  @media screen and (max-width: 767px) {
+  }
 `;
 
 export const Image = styled.img`
   border-radius: 1rem;
   width: 280px;
   height: 207px;
-  @media screen and (max-width: 1024px){
+  @media screen and (max-width: 1024px) {
     width: 100%;
     height: auto;
-}
+  }
 `;
 
 export const H2 = styled.h2`
   font-size: 48px;
   color: #212121;
   line-height: 1;
-  @media screen and (max-width: 1024px){
+  @media screen and (max-width: 1024px) {
     font-size: 48px;
   }
 `;
@@ -98,7 +95,7 @@ export const Strong = styled.strong`
   font-size: 24px;
   color: #fff;
   padding-bottom: 16px;
-  @media screen and (max-width: 1024px){
+  @media screen and (max-width: 1024px) {
     font-size: 18px;
   }
 `;
@@ -106,36 +103,27 @@ export const Strong = styled.strong`
 export const Text = styled.p`
   font-size: 18px;
   /* color: #212121; */
-  color: ${(props) => (props.theme === 'dark' ? '#a1a1aa' : '#454545')};
+  color: ${(props) => (props.theme === "dark" ? "#a1a1aa" : "#454545")};
   max-width: 80%;
-  @media screen and (max-width: 1024px){
+  @media screen and (max-width: 1024px) {
     max-width: 100%;
-}
-
+  }
 `;
 
-export const BoxWrapper = styled.div`
+export const BoxWrapper = styled.div``;
 
-`;
+export const Box = styled.div``;
 
-export const Box = styled.div`
-
-`;
-
-export const Div = styled.div`
-
-`;
+export const Div = styled.div``;
 
 export const Button = styled.button`
-    font-size: 16px;
-    background-color: #212121;
-    border-radius: 9999px;
-    color: #fff;
-    display: flex;
-    width: max-content;
-    padding: 12px 14px;
-    @media screen and (max-width:767px){
-
-}
+  font-size: 16px;
+  background-color: #212121;
+  border-radius: 9999px;
+  color: #fff;
+  display: flex;
+  width: max-content;
+  padding: 12px 14px;
+  @media screen and (max-width: 767px) {
+  }
 `;
-
